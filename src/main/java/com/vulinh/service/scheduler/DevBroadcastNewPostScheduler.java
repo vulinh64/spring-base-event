79644d11fad96e124b3bc6ca794bf4d564635777
Linh Nguyen <nguyenvulinh64@outlook.com>
2025-12-23 17:37:00 +0700

    Initial commit

---
+++ src/main/java/com/vulinh/service/scheduler/DevBroadcastNewPostScheduler.java
@@ -0,0 +1,57 @@
+package com.vulinh.service.scheduler;
+
+import module java.base;
+
+import com.vulinh.data.EventStatus;
+import com.vulinh.data.entity.NewPost;
+import com.vulinh.data.entity.QNewSubscriber;
+import com.vulinh.data.repository.NewPostRepository;
+import com.vulinh.data.repository.NewSubscriberRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+@Profile("!production")
+public class DevBroadcastNewPostScheduler implements NewPostEventScheduler {
+
+  @Getter final NewPostRepository newPostRepository;
+
+  final NewSubscriberRepository newSubscriberRepository;
+
+  @Override
+  public void processNewPostEvents(NewPost post) {
+    var qNewSubscriber = QNewSubscriber.newSubscriber;
+
+    var validSubscribersData =
+        newSubscriberRepository.findAll(
+            qNewSubscriber
+                .id
+                .subscribedUserId
+                .eq(post.getActionUserId())
+                .and(qNewSubscriber.timestamp.lt(post.getTimestamp())));
+
+    if (CollectionUtils.isNotEmpty(validSubscribersData)) {
+      log.info("Broadcasting size: {}", validSubscribersData.size());
+
+      for (int i = 0; i < validSubscribersData.size(); i++) {
+        var data = validSubscribersData.get(i);
+        log.info(
+            "Notifying subscriber #{}: ID = [{}] (username [{}]) about new post (ID = [{}]): [{}] (from user [{}])",
+            i + 1,
+            data.getId().actionUserId(),
+            data.getActionUsername(),
+            post.getId(),
+            post.getTitle(),
+            data.getSubscribedUsername());
+      }
+    }
+
+    newPostRepository.save(post.setStatus(EventStatus.SUCCESS));
+  }
+}
---
+++ src/main/java/com/vulinh/service/scheduler/NewPostEventScheduler.java
@@ -0,0 +1,41 @@
+package com.vulinh.service.scheduler;
+
+import com.vulinh.data.EventStatus;
+import com.vulinh.data.entity.NewPost;
+import com.vulinh.data.entity.QNewPost;
+import com.vulinh.data.repository.NewPostRepository;
+import java.util.concurrent.TimeUnit;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
+import org.springframework.scheduling.annotation.Scheduled;
+
+public interface NewPostEventScheduler {
+
+  int MAX_RETRIES = 5;
+  int LIMIT_ENTRIES = 1;
+  int PAGE_ZERO = 0;
+
+  NewPostRepository getNewPostRepository();
+
+  void processNewPostEvents(NewPost newPost);
+
+  @Scheduled(fixedRate = 10, timeUnit = TimeUnit.SECONDS)
+  default void processNewPostEvent() {
+    var qNewPost = QNewPost.newPost;
+
+    getNewPostRepository()
+        .findAll(
+            qNewPost
+                .status
+                .notIn(EventStatus.SUCCESS, EventStatus.GIVEN_UP)
+                .and(qNewPost.retryCount.loe(MAX_RETRIES)),
+            PageRequest.of(
+                PAGE_ZERO,
+                LIMIT_ENTRIES,
+                Sort.by(Order.asc(qNewPost.timestamp.getMetadata().getName()))))
+        .stream()
+        .findFirst()
+        .ifPresent(this::processNewPostEvents);
+  }
+}
