5e40820e047e8c0b6713ec74a65f05e634b51afc
Linh Nguyen <35946488+vulinh64@users.noreply.github.com>
2025-12-25 17:22:13 +0700

    Test scheduled service
    Yes, I suck at batches, fight me

--- src/main/java/com/vulinh/SpringBaseEventApplication.java
+++ src/main/java/com/vulinh/SpringBaseEventApplication.java
@@ -3,9 +3,11 @@ package com.vulinh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 // Spring Boot main class should have a public constructor if beans are to be defined later
 @SuppressWarnings("java:S1118")
 public class SpringBaseEventApplication {
