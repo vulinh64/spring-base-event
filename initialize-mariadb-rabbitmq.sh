e9bcc6a7d152ebf5a671b5ea32294b95fa020856
Linh Nguyen <nguyenvulinh64@outlook.com>
2026-03-22 13:06:17 +0700

    Update Github CI, dependency versions and image tags

--- initialize-mariadb-rabbitmq.sh
+++ initialize-mariadb-rabbitmq.sh
@@ -2,7 +2,7 @@
 
 RABBITMQ_CONTAINER_NAME="rabbitmq"
 RABBITMQ_NAME="rabbitmq"
-RABBITMQ_TAG="4.2.1-management-alpine"
+RABBITMQ_TAG="4.2.4-alpine"
 RABBITMQ_IMAGE="$RABBITMQ_NAME:$RABBITMQ_TAG"
 RABBITMQ_COMMAND="docker run --detach --name $RABBITMQ_CONTAINER_NAME --hostname rabbitmq -e RABBITMQ_DEFAULT_USER=rabbitmq -e RABBITMQ_DEFAULT_PASS=123456 -p 5672:5672 -p 15672:15672 $RABBITMQ_IMAGE"
 
@@ -21,7 +21,7 @@ fi
 
 MARIADB_CONTAINER_NAME="mariadb"
 MARIADB_NAME="mariadb"
-MARIADB_TAG="12.1.2-noble"
+MARIADB_TAG="12.2.2-noble"
 MARIADB_IMAGE="$MARIADB_NAME:$MARIADB_TAG"
 MARIADB_COMMAND="docker run --detach --name $MARIADB_CONTAINER_NAME -e MARIADB_ROOT_PASSWORD=123456 -e MARIADB_DATABASE=myspringeventdatabase -p 3306:3306 -v mariadb-volume:/var/lib/mysql $MARIADB_IMAGE --lower_case_table_names=1"
 
